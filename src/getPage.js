9b16dcde7c31d7707807144c1ce9cc7415b5766f
Andrea Carraro <me@andreacarraro.it>
2020-09-11 08:54:40 +0200

    Customise req/res mocks via function

--- src/getPage.js
+++ src/getPage.js
@@ -5,12 +5,12 @@ import preparePage from './preparePage';
 export default async function getPage({
   pagesDirectory,
   route,
-  req = {},
-  res = {},
+  req: reqMocker = (req) => req,
+  res: resMocker = (res) => res,
 }) {
   const pageObject = await getPageObject({ pagesDirectory, route });
   if (pageObject) {
-    let pageElement = await fetchData({ pageObject, req, res });
+    let pageElement = await fetchData({ pageObject, reqMocker, resMocker });
     pageElement = preparePage({ pageElement, pageObject });
     return pageElement;
   }
