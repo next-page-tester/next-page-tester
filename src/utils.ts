18cbeb73d99669bcbf9c66beb15d49e0dd3b8e5e
Andrea Carraro <me@andreacarraro.it>
2020-09-26 11:17:06 +0200

    Switch from url to URL

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,11 @@
-const url = require('url');
+import { URL } from 'url';
 import querystring from 'querystring';
 
 export function parseRoute({ route }: { route: string }) {
-  return url.parse(`http://test.com${route}`);
+  return new URL(`http://test.com${route}`);
 }
 
-export function parseQueryString({ queryString }: { queryString?: string }) {
-  if (typeof queryString !== 'string') return {};
+export function parseQueryString({ queryString }: { queryString: string }) {
   const qs = queryString.startsWith('?')
     ? queryString.substring(1)
     : queryString;
