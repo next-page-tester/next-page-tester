0d54ccfd028acb836e86346111c233cbd172e023
Andrea Carraro <toomuchdesign@users.noreply.github.com>
2020-12-07 13:32:42 +0100

    ci: Add Prettier check (#50)
    * Remove some unused files
    * Check code format on prepare
    * add .gitattributes file
    * Change test order
    * Mock console.error only once before tests
    * Clear cookies afterEach test only if needed

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -3,9 +3,7 @@ import '@testing-library/jest-dom';
 beforeAll(() => {
   // Mock global.scrollTo, since Next.js Link component trigger it
   global.scrollTo = jest.fn();
-});
 
-beforeEach(() => {
   // Suppress validateDOMNesting error logs
   // we now we're doing borderline stuff like rendering nested html elements
   const consoleError = console.error;
@@ -16,14 +14,18 @@ beforeEach(() => {
   });
 });
 
-afterEach(() => {
+afterAll(() => {
   // @ts-ignore
   console.error.mockRestore();
+});
 
+afterEach(() => {
   // Clear all cookies
-  document.cookie.split(';').forEach(function (v) {
-    document.cookie = v
-      .replace(/^ +/, '')
-      .replace(/=.*/, '=;expires=' + new Date().toUTCString() + ';path=/');
-  });
+  if (document.cookie !== '') {
+    document.cookie.split(';').forEach(function (v) {
+      document.cookie = v
+        .replace(/^ +/, '')
+        .replace(/=.*/, '=;expires=' + new Date().toUTCString() + ';path=/');
+    });
+  }
 });
