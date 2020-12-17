361f2bfe5901fd0abaed691df0cabf695a154096
Matej Šnuderl <ematej.snuderl@gmail.com>
2020-12-17 09:40:39 +0100

    test: suite for expected global object behaviour

--- src/__tests__/global-object/__fixtures__/pages/gip.js
+++ src/__tests__/global-object/__fixtures__/pages/gip.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { PropsPrinter } from '../../../__utils__';
+
 const window_moduleLoadTime = typeof window !== 'undefined';
 const document_moduleLoadTime = typeof document !== 'undefined';
+const isWeb_moduleLoadTime = window_moduleLoadTime && document_moduleLoadTime;
 
 export default function gip(props) {
+  const window_componentScope = typeof window !== 'undefined';
+  const document_componentScope = typeof document !== 'undefined';
+  const isWeb_componentScope = window_componentScope && document_componentScope;
+
   return (
     <>
       <h2>Page</h2>
       <PropsPrinter
         props={{
           ...props,
-          window_componentScope: typeof window !== 'undefined',
-          document_componentScope: typeof document !== 'undefined',
+          /* Should be hydrated "client" side where browser is defined */
+          window_componentScope,
+          document_componentScope,
+          isWeb_componentScope,
+          window_componentScope_moduleLoadTime: window_moduleLoadTime,
+          document_componentScope_moduleLoadTime: document_moduleLoadTime,
+          isWeb_componentScope_moduleLoadTime: isWeb_moduleLoadTime,
         }}
       />
     </>
@@ -19,10 +30,17 @@ export default function gip(props) {
 }
 
 gip.getInitialProps = async () => {
+  const window_dataFetchingScope = typeof window !== 'undefined';
+  const document_dataFetchingScope = typeof document !== 'undefined';
+  const isWeb_dataFetchingScope =
+    window_dataFetchingScope && document_dataFetchingScope;
+
   return {
     window_moduleLoadTime,
     document_moduleLoadTime,
-    window_dataFetchingScope: typeof window !== 'undefined',
-    document_dataFetchingScope: typeof document !== 'undefined',
+    isWeb_moduleLoadTime,
+    window_dataFetchingScope,
+    document_dataFetchingScope,
+    isWeb_dataFetchingScope,
   };
 };
