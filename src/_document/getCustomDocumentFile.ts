6dad3e592a1caf601f1fd0ff1e4366bf364088eb
Andrea Carraro <me@andreacarraro.it>
2020-12-23 22:06:06 +0100

    Name getCustomDocumentFile function

--- src/_document/getCustomDocumentFile.ts
+++ src/_document/getCustomDocumentFile.ts
@@ -2,7 +2,11 @@ import type { ExtendedOptions, NextCustomDocumentFile } from '../commonTypes';
 import { loadPageIfExists } from '../loadPage';
 import { DOCUMENT_PATH } from '../constants';
 
-export default function getFile({ options }: { options: ExtendedOptions }) {
+export default function getCustomDocumentFile({
+  options,
+}: {
+  options: ExtendedOptions;
+}) {
   return loadPageIfExists<NextCustomDocumentFile>({
     options,
     pagePath: DOCUMENT_PATH,
