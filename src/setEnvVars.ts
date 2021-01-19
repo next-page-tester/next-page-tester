72b23cd9c2f1988d25dc0dc7e7bd712c6ef0b22b
Andrea Carraro <me@andreacarraro.it>
2021-01-19 18:21:39 +0100

    Revert "Provide a logger function to @next/env"
    This reverts commit 1cc86aa84e6e2622887eb739fac64c6c0626f475.

--- src/setEnvVars.ts
+++ src/setEnvVars.ts
@@ -3,17 +3,12 @@ import { RuntimeEnvironment } from './constants';
 import { loadEnvConfig } from '@next/env';
 const { SERVER, CLIENT } = RuntimeEnvironment;
 
-const Logger = {
-  info: () => {},
-  error: () => {},
-};
-
 let dotFile: Record<string, string> | undefined = undefined;
 export function loadDotFile({ nextRoot }: { nextRoot: string }) {
   // Temporarily hide global process.env to prevent "@next/env" from mutating it
   const currentProcessEnv = process.env;
   process.env = {};
-  const { combinedEnv } = loadEnvConfig(nextRoot, undefined, Logger);
+  const { combinedEnv } = loadEnvConfig(nextRoot);
   process.env = currentProcessEnv;
   dotFile = combinedEnv;
 }
