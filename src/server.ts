b6f70e75b0510ad788d822dfbdede11fcf70eff0
Andrea Carraro <me@andreacarraro.it>
2021-01-31 19:17:13 +0100

    refactor: remove duplications in server utils

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,43 @@ import setNextRuntimeConfig from './setNextRuntimeConfig';
 import { setEnvVars } from './setEnvVars';
 import { executeWithFreshModules } from './utils';
 
+function hideBrowserEnv(): () => void {
+  const tmpWindow = global.window;
+  const tmpDocument = global.document;
+
+  // @ts-expect-error its okay
+  delete global.window;
+  // @ts-expect-error its okay
+  delete global.document;
+  setNextRuntimeConfig({ runtimeEnv: RuntimeEnvironment.SERVER });
+  setEnvVars({ runtimeEnv: RuntimeEnvironment.SERVER });
+
+  return () => {
+    global.window = tmpWindow;
+    global.document = tmpDocument;
+    setNextRuntimeConfig({ runtimeEnv: RuntimeEnvironment.CLIENT });
+    setEnvVars({ runtimeEnv: RuntimeEnvironment.CLIENT });
+  };
+}
+
+export const executeAsIfOnServer = async <T>(f: () => T) => {
+  const restoreBrowserEnv = hideBrowserEnv();
+  try {
+    return await f();
+  } finally {
+    restoreBrowserEnv();
+  }
+};
+
+export const executeAsIfOnServerSync = <T>(f: () => T): T => {
+  const restoreBrowserEnv = hideBrowserEnv();
+  try {
+    return f();
+  } finally {
+    restoreBrowserEnv();
+  }
+};
+
 export const requireAsIfOnServer = <FileType>({
   path,
   nonIsolatedModules,
@@ -16,46 +53,4 @@ export const requireAsIfOnServer = <FileType>({
     },
     { nonIsolatedModules }
   );
-};
-
-export const executeAsIfOnServer = async <T>(f: () => T) => {
-  const tmpWindow = global.window;
-  const tmpDocument = global.document;
-
-  // @ts-expect-error its okay
-  delete global.window;
-  // @ts-expect-error its okay
-  delete global.document;
-  setNextRuntimeConfig({ runtimeEnv: RuntimeEnvironment.SERVER });
-  setEnvVars({ runtimeEnv: RuntimeEnvironment.SERVER });
-
-  try {
-    return await f();
-  } finally {
-    global.window = tmpWindow;
-    global.document = tmpDocument;
-    setNextRuntimeConfig({ runtimeEnv: RuntimeEnvironment.CLIENT });
-    setEnvVars({ runtimeEnv: RuntimeEnvironment.CLIENT });
-  }
-};
-
-export const executeAsIfOnServerSync = <T>(f: () => T): T => {
-  const tmpWindow = global.window;
-  const tmpDocument = global.document;
-
-  // @ts-expect-error its okay
-  delete global.window;
-  // @ts-expect-error its okay
-  delete global.document;
-  setNextRuntimeConfig({ runtimeEnv: RuntimeEnvironment.SERVER });
-  setEnvVars({ runtimeEnv: RuntimeEnvironment.SERVER });
-
-  try {
-    return f();
-  } finally {
-    global.window = tmpWindow;
-    global.document = tmpDocument;
-    setNextRuntimeConfig({ runtimeEnv: RuntimeEnvironment.CLIENT });
-    setEnvVars({ runtimeEnv: RuntimeEnvironment.CLIENT });
-  }
 };
