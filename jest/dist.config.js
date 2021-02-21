b148fccd3badf330ee958e431975dc3dfbfbc5d7
Andrea Carraro <me@andreacarraro.it>
2021-02-21 01:51:19 +0100

    fix: jest dist moduleNameMapper configuration

--- jest/dist.config.js
+++ jest/dist.config.js
@@ -3,6 +3,7 @@ var pkg = require('../package.json');
 module.exports = Object.assign({}, pkg.jest, {
   rootDir: '../',
   moduleNameMapper: {
+    ...pkg.jest.moduleNameMapper,
     '/src$': '<rootDir>/dist/index',
   },
 });
