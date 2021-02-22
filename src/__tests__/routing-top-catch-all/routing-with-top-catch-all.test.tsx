c0e87e6ecd57e43e985fcc2c90e94059f6260ddb
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-02-22 20:29:17 +0100

    test: Reduce amount of catch all tests

--- src/__tests__/routing-top-catch-all/routing-with-top-catch-all.test.tsx
+++ src/__tests__/routing-top-catch-all/routing-with-top-catch-all.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { getPage } from '../..';
 import { expectDOMElementsToMatch, renderWithinNextRoot } from '../__utils__';
 import BlogPage from './__fixtures__/pages/blog/[id]';
-import BlogPage99 from './__fixtures__/pages/blog/99';
 import CatchAllPage from './__fixtures__/pages/[...catchAll]';
 
 const nextRoot = __dirname + '/__fixtures__';
@@ -25,75 +24,22 @@ describe('Routing in presence of top level catch all', () => {
     );
     expectDOMElementsToMatch(actual, expected);
   });
+});
 
+describe('Top level catch all', () => {
   it('gets expected page and router object', async () => {
     const { render } = await getPage({
       nextRoot,
-      route: '/blog/5?foo=bar',
+      route: '/catch-all/5/aaa',
     });
     const { nextRoot: actual } = render();
     const { container: expected } = renderWithinNextRoot(
-      <BlogPage
+      <CatchAllPage
         routerMock={{
-          query: {
-            id: '5',
-            foo: 'bar',
-          },
+          query: { catchAll: ['catch-all', '5', 'aaa'] },
         }}
       />
     );
     expectDOMElementsToMatch(actual, expected);
-  });
-
-  it('predefined routes take precedence over dynamic', async () => {
-    const { render } = await getPage({
-      nextRoot,
-      route: '/blog/99',
-    });
-    const { nextRoot: actual } = render();
-    const { container: expected } = renderWithinNextRoot(<BlogPage99 />);
-    expectDOMElementsToMatch(actual, expected);
-  });
-});
-
-describe('Top level catch all', () => {
-  describe('single catch-all param', () => {
-    it('gets expected page and router object', async () => {
-      const { render } = await getPage({
-        nextRoot,
-        route: '/catch-all/5/aaa',
-      });
-      const { nextRoot: actual } = render();
-      const { container: expected } = renderWithinNextRoot(
-        <CatchAllPage
-          routerMock={{
-            query: { catchAll: ['catch-all', '5', 'aaa'] },
-          }}
-        />
-      );
-      expectDOMElementsToMatch(actual, expected);
-    });
-  });
-
-  describe('multiple catch-all params + querystring', () => {
-    it('gets expected page and router object', async () => {
-      const { render } = await getPage({
-        nextRoot,
-        route: '/catch-all/5/aaa/bbb/ccc?foo=bar',
-      });
-      const { nextRoot: actual } = render();
-      const { container: expected } = renderWithinNextRoot(
-        <CatchAllPage
-          routerMock={{
-            query: {
-              catchAll: ['catch-all', '5', 'aaa', 'bbb', 'ccc'],
-              foo: 'bar',
-            },
-          }}
-        />
-      );
-
-      expectDOMElementsToMatch(actual, expected);
-    });
   });
 });
