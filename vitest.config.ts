ebd3011de565bc4f2ff11a75f1883c3780a46a9b
Jason Williams <jwilliams720@bloomberg.net>
2022-02-24 17:00:00 +0000

    try vitest

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+// vite.config.ts
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    watch: false,
+  },
+});
